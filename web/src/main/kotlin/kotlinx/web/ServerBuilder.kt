186f8b6a37c99d0618f65b2ed01df41653abc368
jaysonminard <jayson.minard@gmail.com>
2014-12-11 10:50:23 -0200

    Rough ideas, not useable web stuff

--- web/src/main/kotlin/kotlinx/web/ServerBuilder.kt
+++ web/src/main/kotlin/kotlinx/web/ServerBuilder.kt
@@ -5,6 +5,18 @@ import java.util.LinkedList
 import kotlin.properties.Delegates
 import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.LinkedHashSet
+import io.undertow.Undertow
+import sun.reflect.generics.reflectiveObjects.NotImplementedException
+import kotlinx.jdk.numbers.minimum
+import io.undertow.server.handlers.accesslog.AccessLogHandler
+import io.undertow.server.handlers.accesslog.AccessLogReceiver
+import org.slf4j.LoggerFactory
+import io.undertow.UndertowOptions
+import io.undertow.server.handlers.resource.ResourceHandler
+import io.undertow.server.handlers.resource.FileResourceManager
+import java.io.File
+import io.undertow.server.HttpHandler
+import io.undertow.server.HttpServerExchange
 
 public enum class ServerProtocol {
     HTTP
@@ -14,17 +26,24 @@ public enum class ServerProtocol {
 
 public class ServerBuilder(init: ServerBuilder.() -> Unit = {}) {
     private val _allListeners = arrayListOf<Listener>()
-    public val allListeners: List<Listener>
+    public val listeners: List<Listener>
         get() { return _allListeners }
+
+    private val _allRoutes = arrayListOf<Route>()
+    public val routes: List<Route>
+        get() { return _allRoutes }
+
+    public var accessLogFormat: String = "%t %a %p \"%r\" %q %s %b %Dms"
+    public var ioThreads: Int = Runtime.getRuntime().availableProcessors()
+    public var workerThreads: Int = Runtime.getRuntime().availableProcessors() * 8
 
     ; {
         with (this) { init() }
     }
 
     public fun build(): Server {
-         return Server()
+         return Server(_allListeners, ioThreads, workerThreads, accessLogFormat)
     }
-
 
     private fun checkDefaultListener(): Unit {
         if (_allListeners.isEmpty()) {
@@ -32,30 +51,100 @@ public class ServerBuilder(init: ServerBuilder.() -> Unit = {}) {
         }
     }
 
-    public fun ServerBuilder.listen(init: Listener.() -> Unit): Unit {
+    public fun ServerBuilder.listenOn(init: Listener.() -> Unit): Unit {
         val newListener = Listener()
         this@ServerBuilder._allListeners.add(newListener)
         newListener.init()
     }
 
-    open inner class Listener(var host: String = "0.0.0.0", var port: Int = 8080, var protocol: ServerProtocol = ServerProtocol.HTTP) {
-        public val HTTP: ServerProtocol = ServerProtocol.HTTP
-        public val HTTPS: ServerProtocol = ServerProtocol.HTTPS
-        public val WEBSOCKETS: ServerProtocol = ServerProtocol.WEBSOCKETS
+    public fun <RT: HttpHandler> ServerBuilder.route(pathTemplate: String, handler: RT, init: RT.() -> Unit = {}): Unit {
+        val newRoute = Route(pathTemplate, handler)
+        this@ServerBuilder._allRoutes.add(newRoute)
+        with (handler) { init() }
+    }
 
-        public fun port(port: Int): Listener {
-            this.port = port
-            return this
-        }
 
-        public fun protocol(proto: ServerProtocol): Listener {
-            this.protocol = proto
-            return this
-        }
+
+}
+
+class Listener(var host: String = "0.0.0.0", var port: Int = 8080, var protocol: ServerProtocol = ServerProtocol.HTTP) {
+    public val HTTP: ServerProtocol = ServerProtocol.HTTP
+    public val HTTPS: ServerProtocol = ServerProtocol.HTTPS
+    public val WEBSOCKETS: ServerProtocol = ServerProtocol.WEBSOCKETS
+}
+
+class Route(val pathTemplate: String, val handler: HttpHandler)  { }
+
+
+open public class FileResourceHandler(path: String, transferMinSize: Long = 1024, caseSensitive: Boolean = true, followSymLinks: Boolean = true, followSafePaths: List<String> = listOf(path), welcomeFiles: List<String> = listOf("index.html")): ResourceHandler(FileResourceManager(File(path), transferMinSize, caseSensitive, followSymLinks, *followSafePaths.copyToArray())) {
+    {
+        addWelcomeFiles(*welcomeFiles.copyToArray())
     }
 }
 
-public class Server() {
+open public class Resource() {
+
+}
+
+open public class ErrorResult(val httpCode: Int, val msg: String): Resource() {
+
+}
+
+open public class NullHttpHandler() : HttpHandler {
+    override fun handleRequest(exchange: HttpServerExchange?) {
+        throw UnsupportedOperationException("no handler was defined") // TODO: message with route info
+    }
+
+}
+
+public class Server(val listeners: List<Listener>,
+                    requestedIoThreads: Int,
+                    requestedWorkerThreads: Int,
+                    val accessLogFormat: String) {
+    val accessLogger = LoggerFactory.getLogger("http.access")!!
+
+    val undertow = run {
+        val ioThreads = (if (requestedIoThreads <= 0) Runtime.getRuntime().availableProcessors() else requestedIoThreads).minimum(2)
+        val workerThreads = (if (requestedWorkerThreads <= 0) Runtime.getRuntime().availableProcessors() * 8 else requestedWorkerThreads).minimum(8)
+
+        val rootHandler = ResourceHandler(FileResourceManager(File("/Users/jminard/DEV/Collokia/oss/reakt/todo"), 1024)).addWelcomeFiles("index.html")
+
+        val outerHandler = AccessLogHandler(rootHandler, object : AccessLogReceiver {
+            override fun logMessage(message: String?) {
+                accessLogger.info(message)
+            }
+        }, accessLogFormat, javaClass<Server>().getClassLoader())
+
+        val builder = Undertow.builder()
+        .setDirectBuffers(true)
+        .setIoThreads(ioThreads)
+        .setWorkerThreads(workerThreads)
+        .setHandler(outerHandler)
+        .setServerOption(UndertowOptions.RECORD_REQUEST_START_TIME, true)  // TODO: allow configuration?
+        listeners.forEach { listener ->
+            if (listener.protocol == ServerProtocol.HTTP) {
+                builder.addHttpListener(listener.port, listener.host)
+            }
+            else if (listener.protocol == ServerProtocol.HTTPS) {
+                throw IllegalArgumentException("HTTPS not yet supported")
+            }
+            else if (listener.protocol == ServerProtocol.WEBSOCKETS) {
+                throw IllegalArgumentException("WebSockets not yet supported")
+            }
+            else {
+                throw IllegalArgumentException("Unknown protocol: ${listener.protocol}")
+            }
+        }
+        builder.build()
+    }
+
+    public fun start() {
+        undertow.start()
+    }
+
+    public fun stop() {
+        undertow.stop()
+    }
 
 }
 
--- web/src/test/kotlin/kotlinx/web/ServerBuilder.kt
+++ web/src/test/kotlin/kotlinx/web/ServerBuilder.kt
@@ -6,55 +6,72 @@ import kotlin.test.assertNotNull
 import org.junit.Test
 import kotlin.test.assertTrue
 import kotlin.test.assertFalse
+import io.undertow.server.handlers.resource.ResourceHandler
+import io.undertow.server.handlers.resource.FileResourceManager
+import java.io.File
 
 
 public class ServerBuilderTests {
     Test fun testListeners() {
         isolatedTest {
             val server = ServerBuilder {
-                listen {
+                listenOn {
                     port = 8443
                     host = "127.0.0.1"
                     protocol = HTTPS
                 }
             }
 
-            assertEquals(1, server.allListeners.size())
-            assertEquals("127.0.0.1", server.allListeners[0].host)
-            assertEquals(8443, server.allListeners[0].port)
-            assertEquals(ServerProtocol.HTTPS, server.allListeners[0].protocol)
+            assertEquals(1, server.listeners.size())
+            assertEquals("127.0.0.1", server.listeners[0].host)
+            assertEquals(8443, server.listeners[0].port)
+            assertEquals(ServerProtocol.HTTPS, server.listeners[0].protocol)
         }
 
         isolatedTest {
             val server = ServerBuilder {
-                listen {
+                listenOn {
                     port = 9080
                     host = "0.0.0.0"
                 }
-                listen {
+                listenOn {
                     port = 9443
                     host = "127.0.0.1"
                     protocol = HTTPS
                 }
-                listen {
+                listenOn {
                     port = 9092
                     host = "127.0.0.1"
                     protocol = WEBSOCKETS
                 }
             }
 
-            assertEquals(3, server.allListeners.size())
+            assertEquals(3, server.listeners.size())
 
-            assertEquals("0.0.0.0", server.allListeners[0].host)
-            assertEquals(9080, server.allListeners[0].port)
-            assertEquals(ServerProtocol.HTTP, server.allListeners[0].protocol)
-            assertEquals("127.0.0.1", server.allListeners[1].host)
-            assertEquals(9443, server.allListeners[1].port)
-            assertEquals(ServerProtocol.HTTPS, server.allListeners[0].protocol)
-            assertEquals("127.0.0.1", server.allListeners[2].host)
-            assertEquals(9092, server.allListeners[2].port)
-            assertEquals(ServerProtocol.WEBSOCKETS, server.allListeners[0].protocol)
+            assertEquals("0.0.0.0", server.listeners[0].host)
+            assertEquals(9080, server.listeners[0].port)
+            assertEquals(ServerProtocol.HTTP, server.listeners[0].protocol)
+            assertEquals("127.0.0.1", server.listeners[1].host)
+            assertEquals(9443, server.listeners[1].port)
+            assertEquals(ServerProtocol.HTTPS, server.listeners[0].protocol)
+            assertEquals("127.0.0.1", server.listeners[2].host)
+            assertEquals(9092, server.listeners[2].port)
+            assertEquals(ServerProtocol.WEBSOCKETS, server.listeners[0].protocol)
         }
 
     }
+
+    Test fun testRoutes() {
+        isolatedTest {
+            val server = ServerBuilder {
+                listenOn {
+                    port = 8080
+                }
+                route("/", FileResourceHandler("/some/path")) {
+
+                }
+            }
+        }
+    }
+
 }
