304d97d1f5e2f94d71ff1fe63ee933103b617cd1
jaysonminard <jayson.minard@gmail.com>
2014-12-14 19:22:14 -0200

    making some things easier and less muddy for HTML and CSS

--- web-demo/src/main/kotlin/kara/demo/styles/StyleClasses.kt
+++ web-demo/src/main/kotlin/kara/demo/styles/StyleClasses.kt
@@ -3,7 +3,7 @@ package kara.demo.styles
 import kotlinx.kara.*
 import kotlinx.html.*
 
-public enum class StyleClasses : StyleClass {
+public enum class StyleClasses : StyleEnum {
     fields cLabel top
     date title body
 }
