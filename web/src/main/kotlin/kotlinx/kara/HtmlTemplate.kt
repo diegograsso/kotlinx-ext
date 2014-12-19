ebb0a1e97efa3e5272a53cf04b3c2bb1ef13743a
jaysonminard <jayson.minard@gmail.com>
2014-12-19 09:36:00 -0200

    Allow DIV to be top level item (i.e. for rendering page fragments)

--- web/src/main/kotlin/kotlinx/kara/HtmlTemplate.kt
+++ web/src/main/kotlin/kotlinx/kara/HtmlTemplate.kt
@@ -2,6 +2,8 @@ package kotlinx.kara
 
 import javax.servlet.http.HttpServletResponse
 import kotlinx.html.HTML
+import kotlinx.html.DIV
+import kotlinx.html.StyleClass
 
 public open class HtmlTemplateView<T : Template<HTML>>(val template: T, val build: T.() -> Unit) : ActionResult {
     override fun writeResponse(context: ActionContext) {
@@ -16,6 +18,32 @@ public open class HtmlTemplateView<T : Template<HTML>>(val template: T, val buil
         with(template) {
             with(view) { build() }
             with(page) { render() }
+        }
+        writer.write(page.toString())
+        writer.flush()
+    }
+}
+
+public open class DivTemplateView<T : Template<DIV>>(val template: T, val divId: String? = null, vararg divStyles: StyleClass, val build: T.() -> Unit) : ActionResult {
+    val styles = divStyles
+    override fun writeResponse(context: ActionContext) {
+        writeResponse(context.response)
+    }
+
+    fun writeResponse(response: HttpServletResponse) {
+        response.setContentType("text/html")
+        val writer = response.getWriter()!!
+        val view = this@DivTemplateView
+        val page = DIV()
+        with(template) {
+            with(view) {
+                build()
+            }
+            with(page) {
+                if (view.divId != null) id = view.divId
+                for (style in view.styles) addClass(style)
+                render()
+            }
         }
         writer.write(page.toString())
         writer.flush()
